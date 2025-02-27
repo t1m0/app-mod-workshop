bc1b4b6f5a07809aa34ca4b03407c72e7420a53d
Timo Schoepflin <timo@schoepflin.io>
2025-02-27 14:06:32 +0000

    Initial gcp setup

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
-// Configurazione del database
-$db_host = 'localhost';
-$db_name = 'image_catalog';
-$db_user = 'root';
-$db_pass = 'veryverystrongpassword';
-
+// Database configuration with ENV variables. Set default values as well 
+$db_host = getenv('DB_HOST') ?: 'localhost';
+$db_name = getenv('DB_NAME') ?: 'image_catalog';
+$db_user = getenv('DB_USER') ?: 'appmod-phpapp-user';
+$db_pass = getenv('DB_PASS') ?: 'wrong_password';
+// Note getenv() is PHP 5.3 compatible
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
